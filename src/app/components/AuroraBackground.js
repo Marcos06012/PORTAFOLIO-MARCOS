70939a073d6a73ef8f90bacab52d03f2a64ce128
MARCOS JARED ALAS MORALES <marcos.alas24@itca.edu.sv>
2026-03-11 21:38:16 -0600

    Nuevos Arreglos de Fondo

--- src/app/components/AuroraBackground.js
+++ src/app/components/AuroraBackground.js
@@ -5,11 +5,11 @@ export default function AuroraBackground() {
         <div className="fixed inset-0 -z-10 overflow-hidden bg-black">
 
             {/* Aurora blobs */}
-            <div className="absolute w-[900px] h-[900px] bg-purple-600 rounded-full blur-[250px] opacity-40 animate-aurora top-[-200px] left-[-200px]" />
+            <div className="absolute w-225 h-225 bg-purple-600 rounded-full blur-[250px] opacity-40 animate-aurora -top-50 -left-50" />
 
-            <div className="absolute w-[900px] h-[900px] bg-blue-600 rounded-full blur-[250px] opacity-40 animate-aurora2 bottom-[-300px] right-[-200px]" />
+            <div className="absolute w-225 h-225 bg-blue-600 rounded-full blur-[250px] opacity-40 animate-aurora2 -bottom-75 -right-50" />
 
-            <div className="absolute w-[900px] h-[900px] bg-pink-500 rounded-full blur-[250px] opacity-30 animate-aurora3 top-[40%] left-[30%]" />
+            <div className="absolute w-225 h-225 bg-pink-500 rounded-full blur-[250px] opacity-30 animate-aurora3 top-[40%] left-[30%]" />
 
             {/* Grain effect */}
             <div className="absolute inset-0 opacity-10 mix-blend-overlay pointer-events-none bg-[url('https://grainy-gradients.vercel.app/noise.svg')]" />
